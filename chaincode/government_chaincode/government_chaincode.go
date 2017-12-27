dbbf9194d172809c942e47abd5186fde79c04a4c
ransht <ransht@gmail.com>
2017-12-27 21:04:21 +0200

    government checks

--- chaincode/government_chaincode/government_chaincode.go
+++ chaincode/government_chaincode/government_chaincode.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
+
+	"github.com/hyperledger/fabric/core/chaincode/lib/cid"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
-	"bytes"
-	"github.com/hyperledger/fabric/core/chaincode/lib/cid"
 )
+
+type Request struct {
+	Hash         string          `json:"Hash"`
+	PropertyHash string          `json:"Name"`
+	Buyer        string          `json:"Buyer"`
+	Salary       int             `json:"TotalSupply"`
+	LoanAmount   int             `json:"LoanAmount"`
+	Status       int             `json:"Status"`
+	Statuses     []RequestStatus `json:"RequestStatuses"`
+	Timestamp    int             `json:"Timestamp"`
+}
+
+type RequestStatus struct {
+	Status    int `json:"Status"`
+	Timestamp int `json:"Timestamp"`
+}
 
 type HomelendChaincode struct {
 }
@@ -41,17 +59,86 @@ func (t *HomelendChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response
 	fmt.Println(fmt.Printf("Access log %s %s", identity, mspid))
 
 	if function == "query" {
+
 		return t.query(stub, args[0], args[1])
+	} else if function == "checkHouseOwner" {
+		request := getRequest(args[0])
+		return t.checkHouseOwner(stub, request)
+	} else if function == "checkLien" {
+		request := getRequest(args[0])
+		return t.checkLien(stub, request)
+	} else if function == "checkWarningShot" {
+		request := getRequest(args[0])
+		return t.checkWarningShot(stub, request)
 	}
 
 	fmt.Println("invoke did not find func: " + function) //error
 	return shim.Error("Received unknown function invocation")
 }
 
-func (t *HomelendChaincode) query(stub shim.ChaincodeStubInterface, owner string, property string) pb.Response {
-	fmt.Println(fmt.Sprintf("government started %s %s", owner, property))
+func (t *HomelendChaincode) query(stub shim.ChaincodeStubInterface, arg1 string, arg2 string) pb.Response {
+	fmt.Println(fmt.Sprintf("government started"))
 
 	return shim.Success([]byte("OK"))
+}
+
+func getRequest(requestStr string) *Request {
+	data := &Request{}
+	err := json.Unmarshal([]byte(requestStr), data)
+	if err != nil {
+		str := fmt.Sprintf("Failed to parse JSON: %+v", err)
+		fmt.Println(str)
+		return nil
+	}
+	return data
+}
+
+func (t *HomelendChaincode) checkLien(stub shim.ChaincodeStubInterface, request *Request) pb.Response {
+	fmt.Println(fmt.Sprintf("government checkLien"))
+
+	if request != nil {
+		val := rand.Intn(100)
+		result := val > 95
+		if result {
+			s := make([]byte, 1)
+			return shim.Success(s)
+		}
+		s := make([]byte, 0)
+		return shim.Success(s)
+	}
+	return shim.Error("Request is null")
+}
+
+func (t *HomelendChaincode) checkHouseOwner(stub shim.ChaincodeStubInterface, request *Request) pb.Response {
+	fmt.Println(fmt.Sprintf("government checkHouseOwner"))
+
+	if request != nil {
+		val := rand.Intn(100)
+		result := val > 95
+		if result {
+			s := make([]byte, 1)
+			return shim.Success(s)
+		}
+		s := make([]byte, 0)
+		return shim.Success(s)
+	}
+	return shim.Error("Request is null")
+}
+
+func (t *HomelendChaincode) checkWarningShot(stub shim.ChaincodeStubInterface, request *Request) pb.Response {
+	fmt.Println(fmt.Sprintf("government checkWarningShot"))
+
+	if request != nil {
+		val := rand.Intn(100)
+		result := val > 95
+		if result {
+			s := make([]byte, 1)
+			return shim.Success(s)
+		}
+		s := make([]byte, 0)
+		return shim.Success(s)
+	}
+	return shim.Error("Request is null")
 }
 
 // ===================================================================================
